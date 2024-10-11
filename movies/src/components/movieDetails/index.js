48362a4737f94bcd25810b71aa691299dfae0f2e
JHolland <20102340@mail.wit.ie>
2024-10-11 14:37:12 +0100

    Show movie review excerpts; Refactor Details page.

--- movies/src/components/movieDetails/index.js
+++ movies/src/components/movieDetails/index.js
@@ -1,114 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import Chip from "@mui/material/Chip";
-import Paper from "@mui/material/Paper";
-import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import MonetizationIcon from "@mui/icons-material/MonetizationOn";
-import StarRate from "@mui/icons-material/StarRate";
-import NavigationIcon from "@mui/icons-material/Navigation";
-import Fab from "@mui/material/Fab";
-import Typography from "@mui/material/Typography";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
+import React, { useState } from "react";
+import Drawer from "@mui/material/Drawer"; // Import Drawer
+import Fab from "@mui/material/Fab"; // Import Fab (Floating Action Button)
+import NavigationIcon from "@mui/icons-material/Navigation"; // Import Icon for the button
+import MovieReviews from "../movieReviews"; // Import MovieReviews
 
-const root = {
-  display: "flex",
-  justifyContent: "center",
-  flexWrap: "wrap",
-  listStyle: "none",
-  padding: 1.5,
-  margin: 0,
-};
-const chip = { margin: 0.5 };
-
-const MovieDetails = () => {
-  const { id } = useParams(); // Extract movie ID from URL
-  const [movie, setMovie] = useState(null);
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    // Fetch movie details
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((movie) => setMovie(movie));
-  }, [id]);
-
-  useEffect(() => {
-    // Fetch movie images
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => setImages(json.posters));
-  }, [id]);
-
-  if (!movie) return <div>Loading...</div>; // Add a loading state
+const MovieDetails = ({ movie }) => {  // Pass movie as a prop
+  const [drawerOpen, setDrawerOpen] = useState(false); // State to control Drawer
 
   return (
     <>
-      <Typography variant="h5" component="h3">
-        Overview
-      </Typography>
-
-      <Typography variant="h6" component="p">
-        {movie.overview}
-      </Typography>
-
-      <Paper component="ul" sx={{ ...root }}>
-        <li>
-          <Chip label="Genres" sx={{ ...chip }} color="primary" />
-        </li>
-        {movie.genres.map((g) => (
-          <li key={g.name}>
-            <Chip label={g.name} sx={{ ...chip }} />
-          </li>
-        ))}
-      </Paper>
-
-      <Paper component="ul" sx={{ ...root }}>
-        <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
-        <Chip
-          icon={<MonetizationIcon />}
-          label={`${movie.revenue.toLocaleString()}`}
-        />
-        <Chip
-          icon={<StarRate />}
-          label={`${movie.vote_average} (${movie.vote_count})`}
-        />
-        <Chip label={`Released: ${movie.release_date}`} />
-      </Paper>
-
-      {/* Render movie images */}
-      <ImageList
-        sx={{
-          height: "100vh",
-        }}
-        cols={1}
-      >
-        {images.map((image) => (
-          <ImageListItem key={image.file_path} cols={1}>
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-              alt={image.file_path}
-            />
-          </ImageListItem>
-        ))}
-      </ImageList>
-
+      {/* Floating Action Button (Fab) to trigger the Drawer */}
       <Fab
         color="secondary"
         variant="extended"
+        onClick={() => setDrawerOpen(true)} // Opens Drawer when clicked
         sx={{
-          position: "fixed",
-          bottom: 2,
-          right: 2,
+          position: 'fixed', // Fix position on the page
+          bottom: '1em',
+          right: '1em'
         }}
       >
         <NavigationIcon />
         Reviews
       </Fab>
+
+      {/* Drawer that opens from the top */}
+      <Drawer 
+        anchor="top" 
+        open={drawerOpen} 
+        onClose={() => setDrawerOpen(false)} // Close Drawer when clicking outside
+      >
+        <MovieReviews movie={movie} /> {/* Pass the movie prop to MovieReviews */}
+      </Drawer>
     </>
   );
 };
