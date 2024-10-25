8499bbe106b128261c2576d7b181f18800db7e05
JHolland <20102340@mail.wit.ie>
2024-10-25 21:34:31 +0100

    Fix backward/forward arrows.

--- movies/src/index.js
+++ movies/src/index.js
@@ -5,17 +5,17 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage"; // Import MovieReviewPage
-import SiteHeader from './components/siteHeader';
+import SiteHeader from './components/siteHeader'; // Import SiteHeader
 
 const App = () => {
   return (
     <BrowserRouter>
-      <SiteHeader />
+      <SiteHeader /> {/* Use the SiteHeader component here */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-        <Route path="/reviews/:id" element={<MovieReviewPage />} /> 
+        <Route path="/reviews/:id" element={<MovieReviewPage />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
